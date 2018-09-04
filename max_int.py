ed2db2a9728a69285c8691e8ad7911b3377858b9
baldure <baldure18@ru.is>
2018-09-04 11:19:16 +0000

    initial commit

---
+++ max_int.py
@@ -0,0 +1,14 @@
+
+num_int = int(input("Input a number: "))    # Do not change this line
+num_2_int=num_int
+max_int=0
+while num_2_int > 0:
+
+    if num_2_int > max_int:
+        max_int = num_2_int
+
+    num_2_int = int(input("Input a number: "))
+    
+
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
